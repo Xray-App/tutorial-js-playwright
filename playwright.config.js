e772906a2ab7cf1abbfa47c227848bf9c6c132d7
CMCunha <cristiano.cunha@gmail.com>
2022-07-05 14:41:02 +0100

    Adding new junit reporter and fixing readme.

---
+++ playwright.config.js
@@ -0,0 +1,27 @@
+
+// JUnit reporter config for Xray
+const xrayOptions = {
+  // Whether to add <properties> with all annotations; default is false
+  embedAnnotationsAsProperties: true,
+
+  // By default, annotation is reported as <property name='' value=''>.
+  // These annotations are reported as <property name=''>value</property>.
+  textContentAnnotations: ['test_description'],
+
+  // This will create a "testrun_evidence" property that contains all attachments. Each attachment is added as an inner <item> element.
+  // Disables [[ATTACHMENT|path]] in the <system-out>.
+  embedAttachmentsAsProperty: 'testrun_evidence',
+
+  // Where to put the report.
+  outputFile: './xray-report.xml'
+};
+
+//const config: PlaywrightTestConfig = {
+const config = {
+  use: {
+    screenshot: 'only-on-failure',
+  },
+  reporter: [ ['junit', xrayOptions] ]
+};
+
+module.exports = config;
